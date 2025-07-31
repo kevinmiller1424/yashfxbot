04f0f297a0e7f8f9f8cf5542f8ee18971518e168
kevinmiller1424 <112707091+kevinmiller1424@users.noreply.github.com>
2025-07-31 14:25:35 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import asyncio
-import datetime
 from telegram import Bot
+import datetime
 import pytz
-import os
 
-TOKEN = os.getenv("BOT_TOKEN") or '8360426787:AAH24XoV-2uB-iEMktKlFjtjDWYEInhVNY0'
-CHANNEL_ID = os.getenv("CHANNEL_ID") or '-1002310864226'
+TOKEN = '8360426787:AAH24XoV-2uB-iEMktKlFjtjDWYEInhVNY0'
+CHANNEL_ID = '-1002310864226'
 
 MESSAGE = """
 🧠 Help Us Improve — We Want Your Feedback
@@ -21,23 +20,12 @@ It only takes a few minutes, and it makes a big difference.
 Thanks for helping us make this better.
 """
 
-gst = pytz.timezone('Asia/Dubai')  # GST = UTC+4
-
 async def send_message():
+    gst = pytz.timezone('Asia/Dubai')
     async with Bot(token=TOKEN) as bot:
         await bot.send_message(chat_id=CHANNEL_ID, text=MESSAGE)
-        print(f"✅ Message sent at {datetime.datetime.now(gst).strftime('%Y-%m-%d %H:%M:%S GST')}")
-
-async def main():
-    now_utc = datetime.datetime.utcnow()
-    now_gst = now_utc.replace(tzinfo=pytz.utc).astimezone(gst)
-
-    # Send only if it is exactly 01:00 AM GST on a Friday
-  #  if now_gst.weekday() == 4 and now_gst.hour == 1 and now_gst.minute == 0:
-    if now_gst.weekday() == 3 and now_gst.hour == 14 and now_gst.minute == 23:
-        await send_message()
-    else:
-        print(f"⏰ Not time yet: {now_gst.strftime('%A %H:%M GST')} — skipping.")
+        now = datetime.datetime.now(gst).strftime('%Y-%m-%d %H:%M:%S GST')
+        print(f"✅ Message sent at {now}")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(send_message())
